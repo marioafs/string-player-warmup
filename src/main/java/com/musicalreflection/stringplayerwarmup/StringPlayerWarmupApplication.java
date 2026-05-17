5970edfb3be68f41e5bc9cbcab4a32d911e93dee
Mario Silva <mario.f@ua.pt>
2026-05-17 23:07:09 +0100

    create project foundation and SQLite config

---
+++ src/main/java/com/musicalreflection/stringplayerwarmup/StringPlayerWarmupApplication.java
@@ -0,0 +1,13 @@
+package com.musicalreflection.stringplayerwarmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringPlayerWarmupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringPlayerWarmupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicalreflection/stringplayerwarmup/StringPlayerWarmupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicalreflection.stringplayerwarmup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringPlayerWarmupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
